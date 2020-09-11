b9e2f17cc1a003313f9573d30690caeb06f346f7
uzaisindiko <uzai.sindiko@gmail.com>
2020-09-12 05:09:51 +0700

    Updates

---
+++ precache-manifest.2d5c6031b6f787648671ee0a547f3a40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d36ebf3cf7d165977abe217a6e368f6",
+    "url": "/UzaiSindiko/robofriends.git/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/UzaiSindiko/robofriends.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4baf3091a5a18341faa7",
+    "url": "/UzaiSindiko/robofriends.git/static/css/main.fa96e02c.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/UzaiSindiko/robofriends.git/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/UzaiSindiko/robofriends.git/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4baf3091a5a18341faa7",
+    "url": "/UzaiSindiko/robofriends.git/static/js/main.33d6d5be.chunk.js"
+  },
+  {
+    "revision": "a775b1279e8042abf621",
+    "url": "/UzaiSindiko/robofriends.git/static/js/runtime-main.18750a15.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/UzaiSindiko/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
